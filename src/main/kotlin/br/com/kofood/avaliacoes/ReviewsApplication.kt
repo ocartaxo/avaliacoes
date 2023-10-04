f2af943b2a0b924297d9988c23d8acb9e0b6ed8d
ocartaxo <oacartax@gmail.com>
2023-10-04 00:40:43 -0300

    applicação

---
+++ src/main/kotlin/br/com/kofood/avaliacoes/ReviewsApplication.kt
@@ -0,0 +1,11 @@
+package br.com.kofood.avaliacoes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReviewsApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReviewsApplication>(*args)
+}
