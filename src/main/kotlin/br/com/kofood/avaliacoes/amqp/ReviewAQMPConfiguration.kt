65ad11945dfc0be4a56e5eb0e8328d0ba65634b0
ocartaxo <oacartax@gmail.com>
2023-10-04 00:36:02 -0300

    configurações amqp

---
+++ src/main/kotlin/br/com/kofood/avaliacoes/amqp/ReviewAQMPConfiguration.kt
@@ -0,0 +1,61 @@
+package br.com.kofood.avaliacoes.amqp
+
+import org.springframework.amqp.core.*
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.amqp.rabbit.core.RabbitAdmin
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.ApplicationListener
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ReviewAQMPConfiguration {
+
+    @Bean
+    fun messageConverter() = Jackson2JsonMessageConverter()
+
+    @Bean
+    fun rabbitTemplate(
+        cf: ConnectionFactory,
+        mc: Jackson2JsonMessageConverter
+    ): RabbitTemplate {
+
+        val rabbitTemplate = RabbitTemplate(cf)
+        rabbitTemplate.messageConverter = mc
+
+        return rabbitTemplate
+
+    }
+
+    @Bean
+    fun reviewDetailsQueue(): Queue = QueueBuilder
+        .nonDurable(PAYMENTS_REVIEW_DETAILS_QUEUE)
+        .build()
+
+    @Bean
+    fun fanoutExchange(): FanoutExchange = ExchangeBuilder
+        .fanoutExchange(PAYMENTS_EXCHANGE)
+        .build()
+
+
+    @Bean
+    fun bindPaymentsOrder(): Binding = BindingBuilder
+        .bind(reviewDetailsQueue())
+        .to(fanoutExchange())
+
+
+    @Bean
+    fun rabbitAdmin(cf: ConnectionFactory) = RabbitAdmin(cf)
+
+
+    @Bean
+    fun startRabbitAdmin(rabbitAdmin: RabbitAdmin) =
+        ApplicationListener<ApplicationReadyEvent> { rabbitAdmin.initialize() }
+
+    companion object {
+        const val PAYMENTS_REVIEW_DETAILS_QUEUE = "payments.review-details"
+        const val PAYMENTS_EXCHANGE = "payments.exchange"
+    }
+}
